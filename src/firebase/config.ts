34666fbda4cf6584f7c3598962b0a431e9528733
Daniel Grigoryan <kra1se03@gmail.com>
2025-11-10 00:30:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9505363899-d07fd",
+  "appId": "1:862905775480:web:62e02433eaeb19a6d5448d",
+  "apiKey": "AIzaSyCGJ222ynlbau-JzUEki4IlvaGYO07IyUo",
+  "authDomain": "studio-9505363899-d07fd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "862905775480"
+};
